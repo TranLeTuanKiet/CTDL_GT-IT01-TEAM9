2d1df2b5a5e3c1adcb3b1fdfd6130aa963d9aab9
tohainhat <109420115+tohainhat@users.noreply.github.com>
2022-08-30 23:28:54 +0700

    Add files via upload

---
+++ ToHaiNhat - 2151050304 - CodeC4/ToHaiNhat - 2151050304 - CodeC4-bai1chuong4.cpp
@@ -0,0 +1,203 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+#define COUNT 10
+struct Node
+{
+	int info;
+	Node* left;
+	Node* right;
+};
+Node* root;
+void Init() {
+	root = NULL;
+}
+int Insert_Node_Recursive(int x, Node* q) {
+	if (q->info == x) return 0;
+	else if (q->info > x)
+	{
+		if (q->left == NULL)
+		{
+			Node* p = new Node;
+			p->info = x;
+			p->left = NULL;
+			p->right = NULL;
+			q->left = p;
+			return 1;
+		}
+		else return Insert_Node_Recursive(x, q->left);
+	}
+	else {
+		if (q->right == NULL)
+		{
+			Node* p = new Node;
+			p->info = x;
+			p->left = NULL;
+			p->right = NULL;
+			q->right = p;
+			return 1;
+		}
+		else return Insert_Node_Recursive(x, q->right);
+	}
+}
+void InsertNode(Node*& p, int x) {
+	if (p == NULL)
+	{
+		p = new Node;
+		p->info = x;
+		p->left = NULL;
+		p->right = NULL;
+	}
+	else {
+		if (p->info == x) return;
+		else if (p->info > x) return InsertNode(p->left, x);
+		else return InsertNode(p->right, x);
+	}
+}
+Node* Search(Node* p, int x) {
+	if (p != NULL)
+	{
+		if (p->info == x) return p;
+		else if (x > p->info) return Search(p->right, x);
+		else return Search(p->left, x);
+	}
+	return NULL;
+}
+void SearchStandFor(Node*& p, Node*& q)
+{
+	if (q->left == NULL)
+	{
+		p->info = q->info;
+		p = q;
+		q = q->right;
+	}
+	else SearchStandFor(p, q->left);
+}
+int Delete(Node*& T, int x)
+{
+	if (T == NULL) return 0;
+	if (T->info == x)
+	{
+		Node* p = T;
+		if (T->left == NULL) T = T->right;
+		else if (T->right == NULL) T = T->left;
+		else SearchStandFor(p, T->right);
+		delete p;
+		return 1;
+	}
+	if (T->info < x) return Delete(T->right, x);
+	if (T->info > x) return Delete(T->left, x);
+}
+void DuyetLNR(Node* p)
+{
+	if (p != NULL)
+	{
+		DuyetLNR(p->left);
+		cout << p->info << " ";
+		DuyetLNR(p->right);
+	}
+}
+void DuyetNLR(Node* p) {
+	if (p != NULL)
+	{
+		cout << p->info << " ";
+		DuyetNLR(p->left);
+		DuyetNLR(p->right);
+	}
+}
+void DuyetLRN(Node* p) {
+	if (p != NULL)
+	{
+		DuyetLRN(p->left);
+		DuyetLRN(p->right);
+		cout << p->info << " ";
+	}
+}
+void print2DUtil(Node* p, int space)
+{
+	if (p == NULL) return;
+	space += COUNT;
+	print2DUtil(p->right, space);
+	cout << endl;
+	for (int i = COUNT; i < space; i++) 
+		cout << " ";
+	cout << p->info << "\n";
+	print2DUtil(p->left, space);
+}
+void Process_Tree() {
+	print2DUtil(root, 0);
+}
+int main() {
+	int choice = 0, x, i;
+	Node* p;
+	system("cls");
+	cout << "bai 1 chuong 4" << endl;
+	cout << "1. Khoi tao cay NPTK rong" << endl;
+	cout << "2. Them phan tu" << endl;
+	cout << "3. Tim phan tu co gia tri x" << endl;
+	cout << "4. Xoa phan tu co gia tri x" << endl;
+	cout << "5. Duyet LNR" << endl;
+	cout << "6. Duyet NLR" << endl;
+	cout << "7. Duyet LRN" << endl;
+	cout << "8. Xuat cay NPTK" << endl;
+	cout << "9. Thoat" << endl;
+	do
+	{
+		cout << "chon so: ";
+		cin >> choice;
+		switch (choice)
+		{
+		case 1:
+			Init();
+			cout << "khoi tao thanh cong" << endl;
+			break;
+		case 2:
+			cout << "nhap x can them: ";
+			cin >> x;
+			InsertNode(root, x);
+			cout << "cay NPTK sau khi them: " << endl;
+			Process_Tree();
+			break;
+		case 3:
+			cout << "nhap x can tim: ";
+			cin >> x;
+			p = Search(root, x);
+			if (p != NULL) cout << "tim thay x=" << x << endl;
+			break;
+		case 4:
+			cout << "nhap x can xoa: ";
+			cin >> x;
+			i = Delete(root, x);
+			if (i == 0) cout << "khong tim thay x=" << x << " de xoa" << endl;
+			else {
+				cout << "xoa thanh cong" << endl;
+				cout << "cay NPTK sau khi xoa: " << endl;
+				Process_Tree();
+			}
+			break;
+		case 5:
+			cout << "cay NPTK duyet theo LNR la: ";
+			DuyetLNR(root);
+			break;
+		case 6:
+			cout << "cay NPTK duyet theo NLR la: ";
+			DuyetNLR(root);
+			break;
+		case 7:
+			cout << "cay NPTK duyet theo LRN la: ";
+			DuyetLRN(root);
+			break;
+		case 8:
+			cout << "cay NPTK: " << endl;
+			Process_Tree();
+			break;
+		case 9:
+			cout << "good bye" << endl;
+			break;
+		default:
+			break;
+		}
+	} while (choice != 9);
+	system("pause");
+	return 0;
+}
